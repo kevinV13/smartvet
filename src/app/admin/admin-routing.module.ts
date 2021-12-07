7053f1943d44a8f7af4eaab270e8949b06072e56
Kevin <kevinandresvega13@gmail.com>
2021-12-07 07:11:11 -0500

    Primer commit

---
+++ src/app/admin/admin-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { EditHistoriaclinicaComponent } from './historiasclinicas/edit-historiaclinica/edit-historiaclinica.component';
+import { NewHistoriaclinicaComponent } from './historiasclinicas/new-historiaclinica/new-historiaclinica.component';
+import { HistoriaclinicaListComponent } from './historiasclinicas/historiaclinica-list/historiaclinica-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'HistoriaClinica',
+        component: HistoriaclinicaListComponent,
+      },
+      {
+        path: 'HistoriaClinica/new',
+        component: NewHistoriaclinicaComponent,
+      },
+      {
+        path: 'HistoriaClinica/:id/editar',
+        component: EditHistoriaclinicaComponent,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AdminRoutingModule {}
---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { NewHistoriaclinicaComponent } from './historiasclinicas/new-historiaclinica/new-historiaclinica.component';
+import { HistoriaclinicaListComponent } from './historiasclinicas/historiaclinica-list/historiaclinica-list.component';
+import { EditHistoriaclinicaComponent } from './historiasclinicas/edit-historiaclinica/edit-historiaclinica.component';
+import { LayoutComponent } from './layout/layout.component';
+import { MaterialModule } from '../material/material.module';
+import { FormHistoriaclinicaComponent } from './historiasclinicas/shared/form-historiaclinica/form-historiaclinica.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    LayoutComponent,
+    HistoriaclinicaListComponent,
+    NewHistoriaclinicaComponent,
+    EditHistoriaclinicaComponent,
+    FormHistoriaclinicaComponent,
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
+})
+export class AdminModule {}
